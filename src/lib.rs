aa0cbd01fcbaab9911926a82e3112d5974cd2ad7
Brian Friel <briandfriel@gmail.com>
2021-10-06 16:12:29 -0700

    Complete Alice

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+/// Localnet testing params:
+/// Sollet Address: GLAdjCa38boaXipQzy2fhy1QxnQR9YZLPuDnhumg8bPP
+/// Token Mint Account X: BzVURnS8u564h2K8mwEGJ88C4yqULMtfzFrFqM5Yi76A
+/// Alice X Token Account: 3qxT15LvmQeoAv3oUdEbhEoPNue4fqTVwEpi8gS7ygGU
+/// Token Mint Account Y: 7Gfxf1ZwaTSpqrtapNDoWNPBWjcK3C56WN3ykJqAAVg6
+/// Alice Y Token Account: H3BXiXbCC3eBaS1MeyUweytWXUWpofKrPMnGvWJ2595m
 pub mod instruction;
 pub mod error;
 pub mod processor;
+pub mod state;
 
 #[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
