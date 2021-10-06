3e6bfdd678b5fc6e65b92de4b86a83925c44481d
Brian Friel <briandfriel@gmail.com>
2021-10-05 21:02:39 -0700

    Break: state.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,6 @@
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
-};
+pub mod instruction;
+pub mod error;
+pub mod processor;
 
-entrypoint!(process_instruction);
-fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
-}
-
+#[cfg(not(feature = "no-entrypoint"))]
+pub mod entrypoint;
